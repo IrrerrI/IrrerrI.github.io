14b820ac9de77a3945911e11c8f861e54aa8774f
IrrerrI <136517419+IrrerrI@users.noreply.github.com>
2023-06-22 03:15:40 +0500

    added starter functionality

--- Checkers/app.js
+++ Checkers/app.js
@@ -42,17 +42,83 @@ const createGame =() => {
 createGame()
 
 
-const pick =() =>{
-    isRedTurn = !isRedTurn
-    console.log(isRedTurn)
+const pick = (e) => {
+  draggedPiece = e.target;
+  originSquare = e.target.parentNode.getAttribute('square-id')
+
+};
+
+const dragover = (e) => {
+  e.preventDefault();
 }
+
+const drop = (e) => {
+e.preventDefault();
+const target = e.target
+if(target.classList.contains('square')){
+  targetSquareID = target.getAttribute('square-id')
+  targetSquare = target
+} else if(target.classList.contains('piece')){
+  targetSquareID = target.parentNode.getAttribute('square-id')
+  targetSquare = target.parentNode
+}
+checkValid(targetSquareID, originSquare)
+if (validMove == true){
+  commitMove(targetSquareID, originSquare)
+} 
+//console.log(validMove)
+}
+
+const checkValid = (targetSquareID, originSquare) => {
+  //console.log(originSquare)
+  //console.log(targetSquareID)
+  console.log()
+  
+};
+
+  const commitMove = () => {
+    console.log('committing')
+    targetSquare.innerHTML = ('')
+    targetSquare.appendChild(draggedPiece)
+  isredTurn = !isredTurn
+    setPlayer()
+  }
+
+
+
+
+const setPlayer =() => {switch (isRedTurn) {
+  case true:
+    redPieces.forEach(redPiece => {
+      redPiece.draggable = true;
+    });
+    blackPieces.forEach(blackPiece => {
+      blackPiece.draggable = false;
+    });
+    playerDisplay.innerHTML ='red turn'
+    break;
+    
+  case false:
+    redPieces.forEach(redPiece => {
+      redPiece.draggable = false;
+    });
+    blackPieces.forEach(blackPiece => {
+      blackPiece.draggable = true;
+    });
+    playerDisplay.innerHTML ='black turn'
+    break;
+}
+}
+
+
+
+
 
 const allPieces = document.querySelectorAll('.piece')
 allPieces.forEach(allPiece =>{
-    allPiece.addEventListener('dragstart', pick)})/*
+    allPiece.addEventListener('dragstart', pick)
     allPiece.addEventListener('drop', drop)
-    allPiece.addEventListener('dragover', dragover)
-})*/
+    allPiece.addEventListener('dragover', dragover)})
 
 const blackPieces = Array.from(allPieces).splice(0, 12)
 const redPieces = Array.from(allPieces).splice(12, 25)
