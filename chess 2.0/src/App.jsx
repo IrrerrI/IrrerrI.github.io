0e550d232fa44807dca5b1caa8b0559602bcadaa
IrrerrI <136517419+IrrerrI@users.noreply.github.com>
2023-11-22 00:31:12 +0500

    chessboard rendering

--- chess 2.0/src/App.jsx
+++ chess 2.0/src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 
 function App() {
         const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-        const ranks = [1, 2, 3, 4, 5, 6, 7, 8];
-    
+        const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
       
       
         return (
+          <div className='container'>
           <div className="chessboard">
             {
-              squares = ranks.map(rank =>
+            ranks.map(rank =>
           files.map(file => (
             <div key={`${file}${rank}`} className="square">
               {`${file}${rank}`}
@@ -17,6 +17,7 @@ function App() {
           ))
               
             )}
+            </div>
             </div>
             )}
 
